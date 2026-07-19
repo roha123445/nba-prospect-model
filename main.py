4ee6dcb1b81c70f36ea1e422eaeff57e3e964229
Rohan Braich <rohan.braich11@gmail.com>
2026-07-18 20:01:18 -0400

    Initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+print("=" * 40)
+print("NBA Prospect Model")
+print("=" * 40)
+
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+
+print("Everything is working!")
